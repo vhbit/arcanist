c1e9759ab5d4a410bd69ec69ee89c0f14cfc6823
Valerii Hiora <valerii.hiora@gmail.com>
2025-04-11 19:52:06 +0300

    Fixed crash on landing

--- src/parser/argument/PhutilArgumentParser.php
+++ src/parser/argument/PhutilArgumentParser.php
@@ -490,7 +490,7 @@ final class PhutilArgumentParser extends Phobject {
     if ($workflow->isExecutable()) {
       $workflow->setArgv($this);
       $err = $workflow->execute($this);
-      exit($err);
+      exit($err ?? 0);
     } else {
       return $workflow;
     }
